7890e958adb5c5e003a4f3fb3b2e1902572f1120
chandu1811 <chandu1811@users.noreply.github.com>
2021-01-24 14:26:37 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+'''
+String maipulations
+'''
+youtuber="chandu"
+print("chandu")
+print("subscribe to {} youtube channel".format(youtuber))
+print(f"subscribe to {youtuber} youtube channel")
+name=input("Enter name:")
+college_name=input("Enter college name :")
+age=input("age")
+
+strformat=f"My name is {name} ,i studied in {college_name} ,And my age is {age} ,My hobies are Nothing\
+I live in Miryalaguda nalgonda"
+print(strformat)
+
